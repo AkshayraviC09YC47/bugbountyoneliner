bf201bc42d2a01d13ff69cf3afecc83508677bc2
Akshay Ravi <58636452+AkshayraviC09YC47@users.noreply.github.com>
2024-11-29 23:19:42 +0530

    update subzy command

--- autorecon.py
+++ autorecon.py
@@ -138,14 +138,13 @@ def main():
 
     # Run subzy for finding vulnerable subdomain takeovers using live domains from HTTPX
     print("[+] Running Subzy for finding vulnerable subdomains...")
-    subzy_command = f"subzy run --targets {os.path.join(target_folder, 'httpx_live_domains.txt')} -o {os.path.join(target_folder, 'subzy_results.txt')}"
+    subzy_command = f"subzy run --targets {os.path.join(target_folder, 'httpx_live_domains.txt')} | tee {os.path.join(target_folder, 'subzy_results.txt')}"
     run_command(subzy_command, os.path.join(target_folder, 'subzy_results.txt'))
 
     # Show Subzy results
     with open(os.path.join(target_folder, 'subzy_results.txt'), 'r') as f:
         print("\n[+] Subzy Results:")
         print(f.read())
-
 
     # Run nuclei for vulnerability scanning (exclude ssl, info, and unknown issues)
     print("[+] Running Nuclei for vulnerability scanning...")
